4e060ac5e0ef8164ff1e1fe443ede1349697b513
shelbourn <matthew@shelbourn.io>
2020-05-15 18:00:16 -0700

    No need for onLoad handler.

--- tvlModal.js
+++ tvlModal.js
@@ -1,30 +1,34 @@
 // Get the modal
-const modal = document.getElementById('myModal')
+const tvlModal = document.getElementById('tvlModal')
 
 // Get the button that opens the modal
-const btn = document.getElementById('myBtn')
+// const tbtn = document.getElementById('myBtn')
 
 // Get the <span> element that closes the modal
-const span = document.getElementsByClassName('close')[0]
+const tvlSpan = document.getElementsByClassName('tvlClose')[0]
 
 // Get the onPageLoad element that loads the modal automatically
-// const load = document.getElementById('onPageLoad')
+// const load = document.getElementById('tvlOnPageLoad')
 
-window.onload = () => {
-	// When the user clicks the button, open the modal
-	const loadModal = () => {
-		modal.style.display = 'block'
-	}
-	loadModal()
-}
+// load.onload = () => {
+// 	tvlModal.style.display = 'block'
+// }
+
+// window.onload = () => {
+// 	// When the user clicks the button, open the modal
+// 	const loadModal = () => {
+// 		tvlModal.style.display = 'block'
+// 	}
+// 	loadModal()
+// }
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-	modal.style.display = 'none'
+tvlSpan.onclick = function () {
+	tvlModal.style.display = 'none'
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-	if (event.target == modal) {
-		modal.style.display = 'none'
+	if (event.target == tvlModal) {
+		tvlModal.style.display = 'none'
 	}
 }
