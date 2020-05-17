6e108ab4cf84e0c0744554a76179fe8354403b5e
shelbourn <matthew@shelbourn.io>
2020-05-16 17:48:54 -0700

    Adding JS for handlers

--- tvlModal.js
+++ tvlModal.js
@@ -1,29 +1,17 @@
 // Get the modal
 const tvlModal = document.getElementById('tvlModal')
 
-// Get the button that opens the modal
-// const tbtn = document.getElementById('myBtn')
+// Get the decline button
+const declineButton = document.getElementById('btnDecline')
 
-// Get the <span> element that closes the modal
-// const tvlSpan = document.getElementsByClassName('tvlClose')[0]
+// Get the accept button
+const acceptButton = document.getElementById('btnAccept')
 
 // Get the onPageLoad element that loads the modal automatically
 // const load = document.getElementById('tvlOnPageLoad')
 
 // load.onload = () => {
 // 	tvlModal.style.display = 'block'
-// }
-
-// window.onload = () => {
-// 	// When the user clicks the button, open the modal
-// 	const loadModal = () => {
-// 		tvlModal.style.display = 'block'
-// 	}
-// 	loadModal()
-// }
-// When the user clicks on <span> (x), close the modal
-// tvlSpan.onclick = function () {
-// 	tvlModal.style.display = 'none'
 // }
 
 // When the user clicks anywhere outside of the modal, close it
