2a598deaac6f6fd0100c7b5ab7013ebedb5fd14f
shelbourn <matthew@shelbourn.io>
2020-05-21 14:55:25 -0700

    Changing the cookie used

--- tvlModal.js
+++ tvlModal.js
@@ -14,12 +14,51 @@ declineButton.onclick = () => {
 
 // Setting a cookie to hide the modal for one day
 const hideModalCookie = () => {
-	let now = new Date()
-	let time = now.getTime()
-	let expireTime = time + 86400
-	now.setTime(expireTime)
-	document.cookie = 'verify=true;expires=' + now.toGMTString() + ';path=/'
-	tvlModal.style.display = 'none'
+	const cookieName = 'tvlAgeVerification' // The cookie name
+	const cookieLifetime = 365 // Cookie expiry in days
+
+	// Setting a cookie
+	const _setCookie = (cname, cvalue, exdays) => {
+		let d = new Date()
+		d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000)
+		let expires = 'expires=' + d.toUTCString()
+		document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/'
+	}
+
+	// Retrieving any stored cookie
+	const _getCookie = (cname) => {
+		let name = cname + '='
+		let ca = document.cookie.split(';')
+		for (let i = 0; i < ca.length; i++) {
+			let c = ca[i]
+			while (c.charAt(0) == ' ') {
+				c = c.substring(1)
+			}
+			if (c.indexOf(name) == 0) {
+				return c.substring(name.length, c.length)
+			}
+		}
+		return ''
+	}
+
+	// Should the modal be shown?
+	const _shouldShowPopup = () => {
+		if (_getCookie(cookieName)) {
+			return false
+		} else {
+			return true
+		}
+	}
+
+	// Show the cookie popup on load if not previously accepted
+	if (_shouldShowPopup()) {
+		$('#cookieModal').modal('show')
+	}
+
+	// Modal dismiss btn - consent
+	$('#cookieModalConsent').on('click', function () {
+		_setCookie(cookieName, 1, cookieLifetime)
+	})
 }
 
 // Triggering accept button action
